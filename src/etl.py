cb22bf73ae782f0abcf60b6e6ea4b5e878ee271f
Jonatas Vieira <jonatasssvieira@gmail.com>
2026-05-03 14:42:50 -0300

    Etapas do tratamento

--- src/etl.py
+++ src/etl.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 df = pd.read_csv('../global_power_plant_database.csv',low_memory = False)
 
-
+# 2. Criada uma lista de traduções
 traducoes = {
     'country_long': 'Pais',
     'name': 'Nome_da_Usina',
@@ -13,10 +13,13 @@ traducoes = {
     'owner': 'Proprietario'
 }
 
+# 3. Arquivo renomeado
 df_tratado = df[list(traducoes.keys())].rename(columns=traducoes)
 
+# 4. Aqui foi eleiminado os arquivos nulos
 df_tratado.dropna(subset=['Pais', 'Capacidade_MW', 'Latitude', 'Longitude'], inplace=True)
 
+# 5. Criada uma lista de tradução para combustíveis
 dicionario_combustivel = {
     'Hydro': 'Hidrica',
     'Solar': 'Solar',
@@ -28,7 +31,7 @@ dicionario_combustivel = {
     'Waste': 'Residuos',
     'Biomass': 'Biomassa'
 }
-
+ # 6. Combustíveis renomeados
 df_tratado['Tipo_de_Combustivel'] = df_tratado['Tipo_de_Combustivel'].map(dicionario_combustivel).fillna(df_tratado['Tipo_de_Combustivel'])
 
 df_tratado.to_csv('usinas_limpas_faculdade.csv', index=False, encoding='utf-8-sig')
